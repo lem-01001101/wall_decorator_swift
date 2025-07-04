2794a297a719e360ae5e266ce5dfd1205045287d
Leo <leo.magtibay.dev@gmail.com>
2025-07-03 20:45:01 -0700

    Initial Commit

---
+++ WallDecorator/WallDecoratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  WallDecoratorApp.swift
+//  WallDecorator
+//
+//  Created by Leo Magtibay on 7/3/25.
+//
+
+import SwiftUI
+
+@main
+struct WallDecoratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
